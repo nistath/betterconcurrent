01be8082c5b8a7550934421717bded54b449da51
Nick Stathas <nistath@mit.edu>
2023-02-26 12:58:12 -0800

    [test] Improve continuation tests

--- tests/continuation_test.py
+++ tests/continuation_test.py
@@ -4,10 +4,11 @@ from betterconcurrent import ThreadPoolExecutor
 def wait_on_future(executor):
     f = executor.submit(pow, 5, 2)
     yield from f.yield_until_done()
-    print(f.result())
+    return f.result()
 
 
 if __name__ == '__main__':
     with ThreadPoolExecutor(max_workers=1) as executor:
         f = executor.submit(wait_on_future, executor)
+        assert f.result() == 25
         executor.join()
